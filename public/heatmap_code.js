cffb638a9f0439c891e918e372ba43082702593c
Charlie Carson <charliecarson@gmail.com>
2023-10-11 20:45:13 -0500

    use the src/graphQL.js instead of inlining

--- public/heatmap_code.js
+++ public/heatmap_code.js
@@ -1,3 +1,5 @@
+import graphQL from "../src/graphQL.js";
+
 let map, heatmap;
 
 export async function initMap() {
@@ -104,50 +106,11 @@ function changeOpacity() {
   heatmap.set("opacity", opacity);
 }
 
-class HTTPResponseError extends Error {
-	constructor(response) {
-		super(`HTTP Error Response: ${response.status} ${response.statusText}`);
-		this.response = response;
-	}
-}
-
-const checkStatus = response => {
-	if (response.ok) {
-		// response.status >= 200 && response.status < 300
-		return response;
-	} else {
-		throw new HTTPResponseError(response);
-	}
-}
-
-async function graphQL(query, key) {
-  const url = `/graphQL`;
-  const body = JSON.stringify({ query });
-
-  let response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body,
-  });
-  try {
-    checkStatus(response);
-    let json = await response.json();
-    return json.data[key];
-  } catch (error) {
-    console.error(error);
-
-    const errorBody = await error.response.text();
-    console.error(`Error body: ${errorBody}`);
-  }
-}
-
 async function getCoordinates() {
   const query = `{households(ids: []) { latlng }}`;
 
-  let households = await graphQL(query, 'households');
+  const json = await graphQL(query, 'households');
+  let { households } = json.data;
   let coordinates = households
     .map( h => h.latlng)
     .filter( c => c && true)
