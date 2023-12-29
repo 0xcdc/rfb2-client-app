e082b640c34b5e9ee3e57113ab9742e6999f0aea
Charlie Carson <charliecarson@gmail.com>
2023-12-28 21:11:35 -0500

    ignore the bundle size warning

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [preact()],
   build: {
+    chunkSizeWarningLimit: 2000,
     target: 'esnext',
     rollupOptions: {
       input: {
