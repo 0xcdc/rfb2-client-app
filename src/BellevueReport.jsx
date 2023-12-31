408fc4f756b032430be2c7ec608e18146812aaca
Charlie Carson <charliecarson@gmail.com>
2023-12-30 21:46:19 -0500

    remove unused function

--- src/BellevueReport.jsx
+++ src/BellevueReport.jsx
@@ -247,17 +247,6 @@ class BellevueReport extends Component {
     this.aggregateData(this.state.year);
   }
 
-  downloadToFile = (content, filename, contentType) => {
-    const a = document.createElement('a');
-    const file = new Blob([content], { type: contentType });
-
-    a.href= URL.createObjectURL(file);
-    a.download = filename;
-    a.click();
-
-    URL.revokeObjectURL(a.href);
-  }
-
   saveWorksheet = async () => {
     const { year } = this.state;
     if (! this.dataCache[year] ) {
