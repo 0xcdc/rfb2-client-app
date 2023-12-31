e3ee2febce8acf92e2be8871da3eee1f0f8fa81e
Charlie Carson <charliecarson@gmail.com>
2023-12-30 21:22:04 -0500

    removed cruft

--- vite.config.js
+++ vite.config.js
@@ -6,26 +6,7 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [preact()],
   build: {
-    chunkSizeWarningLimit: 2000,
     target: 'esnext',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      /**
-       * Ignore "use client" waning since we are not using SSR
-       * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
-       */
-      onwarn(warning, warn) {
-        if (
-          warning.code === "MODULE_LEVEL_DIRECTIVE" &&
-          warning.message.includes(`"use client"`)
-        ) {
-          return;
-        }
-        warn(warning);
-      },
-    },
   },
   server: {
     port: 3000,
